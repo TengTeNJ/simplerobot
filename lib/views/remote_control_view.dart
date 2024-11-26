c8c059bb4bcce67bc183a48c78681f4ca4bdb02b
App攻城狮 <1215933389@qq.com>
2024-11-26 13:28:59 +0800

    轮盘调整
    轮盘增加长按控制机器人移动

--- lib/views/remote_control_view.dart
+++ lib/views/remote_control_view.dart
@@ -4,6 +4,7 @@ import 'package:tennis_robot/utils/ble_send_util.dart';
 import 'package:tennis_robot/utils/color.dart';
 import 'dart:math' as math;
 import 'package:tennis_robot/utils/robot_manager.dart';
+import 'package:vibration/vibration.dart';
 
 class RemoteControlView extends StatefulWidget {
   const RemoteControlView({super.key});
@@ -17,6 +18,31 @@ class _RemoteControlViewState extends State<RemoteControlView> {
   bool isMove = false; // 滑动标识
   int index = 0;// 索引，记录第一次拖动的方向
   Offset firstPosition = Offset(0, 0);
+
+  int _topImageIndex = 0;
+  List<String> _topImages = [
+    'images/control/control_top.png',
+    'images/control/control_top_high.png'
+  ];
+
+  int _bottomImageIndex = 0;
+  List<String> _bottomImages = [
+    'images/control/control_bottom.png',
+    'images/control/control_bottom_high.png'
+  ];
+
+  int _leftImageIndex = 0;
+  List<String> _leftImages = [
+    'images/control/control_left.png',
+    'images/control/control_left_high.png'
+  ];
+
+  int _rightImageIndex = 0;
+  List<String> _rightImages = [
+    'images/control/control_right.png',
+    'images/control/control_right_high.png'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,10 +55,95 @@ class _RemoteControlViewState extends State<RemoteControlView> {
       child: Stack(
         children: [
           Center(
-            child: Image(
-              image: AssetImage('images/connect/remote_control.png'),
-              height: Constants.screenWidth(context) - 40*3 - 96,
+            // child: Image(
+            //   image: AssetImage('images/connect/remote_control.png'),
+            //   height: Constants.screenWidth(context) - 40*3 - 96,
+            // ),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 20,
+                  left: (Constants.screenWidth(context) - 40*3)/2 - 20,
+                  child: GestureDetector(
+                    onLongPress: () {
+                      BleSendUtil.setRobotAngle(0);
+                      setState(() {
+                        _topImageIndex = 1;
+                        Vibration.vibrate();
+                      });
+                    },
+                    onLongPressUp: (){
+                      setState(() {
+                        _topImageIndex = 0;
+                        Vibration.cancel();
+                      });
+                      },
+                    child: Image.asset('${_topImages[_topImageIndex]}',width: 40,height: 30), // 替换为你的图片路径
+                  ),
+                ),
+                Positioned(
+                  top: (Constants.screenWidth(context) - 40*3) - 30 - 20,
+                  right: (Constants.screenWidth(context) - 40*3)/2 - 20,
+                  child: GestureDetector(
+                    onLongPress: () {
+                      BleSendUtil.setRobotAngle(180);
+                      setState(() {
+                        _bottomImageIndex = 1;
+                        Vibration.vibrate();
+                      });
+                    },
+                    onLongPressUp: (){
+                      setState(() {
+                        _bottomImageIndex = 0;
+                        Vibration.cancel();
+                      });
+                    },
+                    child: Image.asset('${_bottomImages[_bottomImageIndex]}',width: 40,height: 30), // 替换为你的图片路径
+                  ),
+                ),
+                Positioned(
+                  bottom: (Constants.screenWidth(context) - 40*3)/2 - 20,
+                  left: 20,
+                  child: GestureDetector(
+                    onLongPress: () {
+                      setState(() {
+                        BleSendUtil.setRobotAngle(270);
+                        _leftImageIndex = 1;
+                        Vibration.vibrate();
+                      });
+                    },
+                    onLongPressUp: (){
+                      setState(() {
+                        Vibration.cancel();
+                        _leftImageIndex = 0;
+                      });
+                    },
+                    child: Image.asset('${_leftImages[_leftImageIndex]}',width: 30,height: 40), // 替换为你的图片路径
+                  ),
+                ),
+                Positioned(
+                  bottom: (Constants.screenWidth(context) - 40*3)/2 - 20,
+                  right:20,
+                  child: GestureDetector(
+                    onLongPress: () {
+                      setState(() {
+                        BleSendUtil.setRobotAngle(90);
+                        _rightImageIndex = 1;
+                        Vibration.vibrate();
+                      });
+                    },
+                    onLongPressUp: (){
+                      setState(() {
+                        Vibration.cancel();
+                        _rightImageIndex = 0;
+                      });
+                    },
+                    child: Image.asset('${_rightImages[_rightImageIndex]}',width: 30,height: 40), // 替换为你的图片路径
+                  ),
+                ),
+              ],
             ),
+
           ),
           Positioned(
               left: (Constants.screenWidth(context) - 40*3 - 44) / 2.0,
@@ -42,18 +153,16 @@ class _RemoteControlViewState extends State<RemoteControlView> {
                   setState(() {
                     // 开始拖拽
                     isMove = true;
-                    var angle = math.atan2(position.dy, position.dx);
-                    var degrees = angle * (180 / math.pi) + 90;
-
                   });
                 },
+                behavior: HitTestBehavior.opaque,
                 onPanUpdate: (details) {
                   // 更新圆点的位置
                   setState(() {
                     position += details.delta;
                     // 限制圆点在试图A内部移动
                     position = _clampOffsetToCircle(position, ((Constants.screenWidth(context) - 40*3) / 2.0));
-                    print('位置信息为${position}');
+                   // print('位置信息为${position}');
                     // var angle = math.atan2(position.dy, position.dx);
                     // var degrees = angle * (180 / math.pi) + 90;
                     // print('拖动时轮盘的角度${degrees}');
@@ -82,7 +191,7 @@ class _RemoteControlViewState extends State<RemoteControlView> {
                     if (degrees < 0) {
                       degrees = degrees + 360;
                     }
-                    print('角度666${degrees.toInt()}');
+                    print('${getCurrentTime()}---角度${degrees.toInt()}');
                     BleSendUtil.setRobotAngle(degrees.toInt());
 
                   });
@@ -112,6 +221,16 @@ class _RemoteControlViewState extends State<RemoteControlView> {
         ],
       ),
     );
+  }
+
+  String getCurrentTime() {
+    // 获取当前时间
+    DateTime now = DateTime.now();
+    // 格式化时间为字符串
+    String timeString = "${now.hour}:${now.minute}:${now.second}:${now.millisecond}";
+    // 打印时间
+    // print(timeString);
+    return timeString;
   }
   /*限制圆环在圆形内拖拽*/
   Offset _clampOffsetToCircle(Offset offset, double radius) {
