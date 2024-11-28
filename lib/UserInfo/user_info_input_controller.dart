ae506aa79f61255dd0d2b6db2c7f998b5efeda90
App攻城狮 <1215933389@qq.com>
2024-11-28 11:08:25 +0800

    用户输入界面
    fix

--- lib/UserInfo/user_info_input_controller.dart
+++ lib/UserInfo/user_info_input_controller.dart
@@ -26,135 +26,143 @@ class _UserInfoInputControllerState extends State<UserInfoInputController> {
         backgroundColor: Constants.darkControllerColor,
         body: SingleChildScrollView(
           child: ClipRect(
-            child: Container(
-              color: Constants.darkControllerColor,
-              child: Column(
-                children: [
-                  Container(
-                    height: 171,
-                    width: 257,
-                    margin: EdgeInsets.only(top: 167),
-                    child: Image(
-                      image: AssetImage('images/connect/robot.png'),
-                      fit: BoxFit.fill,
+            child: GestureDetector(onTap: (){
+              FocusScope.of(context).unfocus();
+
+            },
+                    child: Container(
+                      color: Constants.darkControllerColor,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 171,
+                            width: 257,
+                            margin: EdgeInsets.only(top: 167),
+                            child: Image(
+                              image: AssetImage('images/connect/robot.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Container(
+                            // color: Colors.red,
+                            width: Constants.screenWidth(context),
+                            margin: EdgeInsets.only(left: 70, right: 44, top: 50),
+                            child: Constants.regularWhiteTextWidget('NickName', 18, Colors.white,textAlign: TextAlign.left),
+                          ),
+
+                          Container(
+                            margin: EdgeInsets.only(left: 70, right: 44, top: 4),
+                            height:44 ,
+                            child: TextField(
+                              cursorColor: Color.fromRGBO(248, 98, 21, 1),
+                              onChanged:(value) {
+                                if (_emailController.text != '' && _nickController.text != ''){
+                                  setState(() {
+                                    isConnected = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    isConnected = false;
+                                  });
+                                }
+                              },
+                              controller: _nickController,
+                              style: TextStyle(color: Constants.baseStyleColor), // 设置字体颜色
+                              decoration: InputDecoration(
+                                hintText: 'Enter Nickname', // 占位符文本
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Color.fromRGBO(248, 98, 21, 1)), // 设置焦点时的边框颜色
+                                ),
+                                hintStyle: TextStyle(
+                                    color: Color.fromRGBO(156, 156, 156, 1.0),
+                                    fontFamily: 'SanFranciscoDisplay',
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+
+                          Container(
+                            // color: Colors.red,
+                            width: Constants.screenWidth(context),
+                            margin: EdgeInsets.only(left: 70, right: 44, top: 40),
+                            child: Constants.regularWhiteTextWidget('Email', 18, Colors.white,textAlign: TextAlign.left),
+                          ),
+
+                          Container(
+                            margin: EdgeInsets.only(left: 70, right: 44, top: 4),
+                            height:44 ,
+                            child: TextField(
+                              onChanged:(value) {
+                                if (_emailController.text != '' && _nickController.text != ''){
+                                  setState(() {
+                                    isConnected = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    isConnected = false;
+                                  });
+                                }
+                              },
+                              controller: _emailController,
+                              cursorColor: Color.fromRGBO(248, 98, 21, 1),
+                              style: TextStyle(color: Constants.baseStyleColor), // 设置字体颜色
+                              decoration: InputDecoration(
+                                hintText: 'Enter Email', // 占位符文本
+                                focusColor: Colors.red,
+                                // enabledBorder: UnderlineInputBorder(
+                                //   borderSide: BorderSide(color: Colors.white), // 设置焦点之外的边框颜色
+                                // ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Color.fromRGBO(248, 98, 21, 1)), // 设置焦点时的边框颜色
+                                ),
+
+
+                                hintStyle: TextStyle(
+                                    color: Color.fromRGBO(156, 156, 156, 1.0),
+                                    fontFamily: 'SanFranciscoDisplay',
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+
+                          GestureDetector(onTap: (){
+                            var nick = _nickController.text;
+                            var email = _emailController.text;
+                            if (!StringUtil.isValidEmail(email)) {
+                              // EasyLoading.showSuccess('Please enter a valid email');
+                              // EasyLoading.showInfo('Please enter a valid email');
+                              EasyLoading.showError('Please enter a valid email');
+                              return;
+                            }
+                            if(nick == ''){
+                              EasyLoading.showError('Please enter nickname');
+                              return;
+                            }
+                            NavigatorUtil.push(Routes.action);
+                          },
+                          child: Container(
+                              child: Center(
+                              child: Constants.mediumWhiteTextWidget(
+                              'Start', 20,isConnected ? Colors.white : Constants.grayTextColor),
+                    ),
+                    height: 72,
+                    margin: EdgeInsets.only(left: 44, right: 44, top: 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(36),
+                      color: isConnected ? Constants.selectedModelOrangeBgColor : Constants.selectModelBgColor ,
                     ),
                   ),
-                  Container(
-                    // color: Colors.red,
-                    width: Constants.screenWidth(context),
-                    margin: EdgeInsets.only(left: 70, right: 44, top: 50),
-                    child: Constants.regularWhiteTextWidget('NickName', 18, Colors.white,textAlign: TextAlign.left),
-                  ),
+                ),
 
-                  Container(
-                    margin: EdgeInsets.only(left: 70, right: 44, top: 4),
-                    height:44 ,
-                    child: TextField(
-                      cursorColor: Color.fromRGBO(248, 98, 21, 1),
-                      onChanged:(value) {
-                        if (_emailController.text != '' && _nickController.text != ''){
-                          setState(() {
-                            isConnected = true;
-                          });
-                        } else {
-                          setState(() {
-                            isConnected = false;
-                          });
-                        }
-                      },
-                      controller: _nickController,
-                      style: TextStyle(color: Constants.baseStyleColor), // 设置字体颜色
-                      decoration: InputDecoration(
-                        hintText: 'Enter Nickname', // 占位符文本
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Color.fromRGBO(248, 98, 21, 1)), // 设置焦点时的边框颜色
-                        ),
-                        hintStyle: TextStyle(
-                            color: Color.fromRGBO(156, 156, 156, 1.0),
-                            fontFamily: 'SanFranciscoDisplay',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400),
+
+              SizedBox(height: 30),
+                        ],
                       ),
-
-
                     ),
-                  ),
-
-                  Container(
-                    // color: Colors.red,
-                    width: Constants.screenWidth(context),
-                    margin: EdgeInsets.only(left: 70, right: 44, top: 40),
-                    child: Constants.regularWhiteTextWidget('Email', 18, Colors.white,textAlign: TextAlign.left),
-                  ),
-
-                  Container(
-                    margin: EdgeInsets.only(left: 70, right: 44, top: 4),
-                    height:44 ,
-                    child: TextField(
-                      onChanged:(value) {
-                        if (_emailController.text != '' && _nickController.text != ''){
-                          setState(() {
-                            isConnected = true;
-                          });
-                        } else {
-                          setState(() {
-                            isConnected = false;
-                          });
-                        }
-                      },
-                      controller: _emailController,
-                      cursorColor: Color.fromRGBO(248, 98, 21, 1),
-                      style: TextStyle(color: Constants.baseStyleColor), // 设置字体颜色
-                      decoration: InputDecoration(
-                        hintText: 'Enter Email', // 占位符文本
-                        focusColor: Colors.red,
-                        // enabledBorder: UnderlineInputBorder(
-                        //   borderSide: BorderSide(color: Colors.white), // 设置焦点之外的边框颜色
-                        // ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Color.fromRGBO(248, 98, 21, 1)), // 设置焦点时的边框颜色
-                        ),
-
-
-                        hintStyle: TextStyle(
-                            color: Color.fromRGBO(156, 156, 156, 1.0),
-                            fontFamily: 'SanFranciscoDisplay',
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ),
-
-                  GestureDetector(onTap: (){
-                    var nick = _nickController.text;
-                    var email = _emailController.text;
-                    if (!StringUtil.isValidEmail(email)) {
-                      // EasyLoading.showSuccess('Please enter a valid email');
-                      // EasyLoading.showInfo('Please enter a valid email');
-                      EasyLoading.showError('Please enter a valid email');
-                      return;
-                    }
-                    NavigatorUtil.push(Routes.action);
-                  },
-                  child: Container(
-                      child: Center(
-                      child: Constants.mediumWhiteTextWidget(
-                      'Start', 20,isConnected ? Colors.white : Constants.grayTextColor),
             ),
-            height: 72,
-            margin: EdgeInsets.only(left: 44, right: 44, top: 60),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(36),
-              color: isConnected ? Constants.selectedModelOrangeBgColor : Constants.selectModelBgColor ,
-            ),
-          ),
-        ),
 
-
-      SizedBox(height: 30),
-                ],
-              ),
-            ),
           ),
         )
     );
