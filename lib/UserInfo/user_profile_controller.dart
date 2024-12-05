a3ab5f35d39537a71deac9d26a3fa6f7b40a9c3d
App攻城狮 <1215933389@qq.com>
2024-12-05 09:50:18 +0800

    个人信息
    个人信息编辑

--- lib/UserInfo/user_profile_controller.dart
+++ lib/UserInfo/user_profile_controller.dart
@@ -16,6 +16,8 @@ class _UserProfileControllerState extends State<UserProfileController> {
 
   var nickName = '';
   var email = '';
+  final TextEditingController _nickController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   @override
   void initState() {
@@ -37,163 +39,220 @@ class _UserProfileControllerState extends State<UserProfileController> {
     setState(() {});
   }
 
+  static Size boundingTextSize(BuildContext context, String text, TextStyle style,  {int maxLines = 2^31, double maxWidth = double.infinity}) {
+    if (text == null || text.isEmpty) {
+      return Size.zero;
+    }
+    final TextPainter textPainter = TextPainter(
+        textDirection: TextDirection.ltr,
+        // locale: Localizations.localeOf(context, null Ok: true),
+        locale: Localizations.localeOf(context),
+        text: TextSpan(text: text, style: style), maxLines: maxLines)
+      ..layout(maxWidth: maxWidth);
+    print('文字高度${textPainter.size.height}');
+    return textPainter.size;
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Constants.darkControllerColor,
       body: ClipRRect(
         borderRadius: BorderRadius.circular(30),
-        child: Container(
-          width:Constants.screenWidth(context),
-          color: Constants.dialogBgColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 36,
-                margin: EdgeInsets.only(right: 16, top: 16),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                   GestureDetector(onTap: () {
-                     NavigatorUtil.pop();
-                     },
-                    child: Text('   Save',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color.fromRGBO(248, 98, 21, 1),
-                        fontSize: 16,
+        child: GestureDetector(onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+          child: Container(
+            width:Constants.screenWidth(context),
+            color: Constants.dialogBgColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 36,
+                  margin: EdgeInsets.only(right: 16, top: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(onTap: () {
+                        var nick = _nickController.text;
+                        var email = _emailController.text;
+                        print('bbb${nick}');
+                        print('bbb${email}');
+
+                        NavigatorUtil.pop();
+                      },
+                        child: Text('   Save',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color.fromRGBO(248, 98, 21, 1),
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
-                    ),
-                   ),
-                    Constants.boldWhiteTextWidget('Profile', 22),
-                    Text('')
-                  ],
+                      Constants.boldWhiteTextWidget('Profile', 22),
+                      Text('')
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(width: Constants.screenWidth(context),height: 1,),
+                SizedBox(width: Constants.screenWidth(context),height: 1,),
 
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                width:  Constants.screenWidth(context),
-                height: 1,
-                color: Color.fromRGBO(86, 89, 101, 1),
-              ),
-
-              Container(
-                margin: EdgeInsets.only(top: 60),
-                width: 183 ,
-                height: 123 ,
-                child: Image(
-                  image: AssetImage('images/connect/robot.png'),
-                  // fit: BoxFit.fill,
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width:  Constants.screenWidth(context),
+                  height: 1,
+                  color: Color.fromRGBO(86, 89, 101, 1),
                 ),
-              ),
 
-              Container(
-                width: Constants.screenWidth(context)- 84,
-                height: 32,
-                margin: EdgeInsets.only(top: 0),
-                child: Center(
-                    child:
-                    Constants.mediumWhiteTextWidget('S1ME01', 20, Colors.white)
+                Container(
+                  margin: EdgeInsets.only(top: 60),
+                  width: 183 ,
+                  height: 123 ,
+                  child: Image(
+                    image: AssetImage('images/connect/robot.png'),
+                    // fit: BoxFit.fill,
+                  ),
                 ),
-              ),
 
-              Container(
-                width: Constants.screenWidth(context)- 140,
-                height: 1,
-                margin: EdgeInsets.only(top: 30),
-                color: Color.fromRGBO(76, 78, 90, 1),
-              ),
-
-              SizedBox(height: 32,),
-              Constants.regularWhiteTextWidget('NickName', 14, Constants.grayTextColor),
-
-              SizedBox(height: 2,),
-
-              Container(
-                // width: Constants.screenWidth(context),
-                // height: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center ,
-                  children: [
-                    Center(
-
-                      // child: TextField(
-                      //   cursorColor: Color.fromRGBO(248, 98, 21, 1),
-                      //   controller: TextEditingController(),
-                      //
-                      //   style: TextStyle(color: Colors.white), // 设置字体颜色
-                      //   decoration: InputDecoration(
-                      //
-                      //     hintText: '${nickName}',
-                      //     hintStyle: TextStyle(
-                      //       color: Color.fromRGBO(255, 255, 255, 1.0),
-                      //       fontFamily: 'SanFranciscoDisplay',
-                      //       fontSize: 18,
-                      //       fontWeight: FontWeight.w500),
-                      //   ),
-                      // ),
-                     child:Constants.mediumWhiteTextWidget('${nickName}', 18, Colors.white),
-                    ),
-                   SizedBox(width: 8,),
-                   GestureDetector(onTap: (){
-                     print('1234');
-                   },
-                   child:Image(image: AssetImage('images/profile/user_edit_info_icon.png'),width: 13,),
-                   )
-                  ],
-
+                Container(
+                  width: Constants.screenWidth(context)- 84,
+                  height: 32,
+                  margin: EdgeInsets.only(top: 0),
+                  child: Center(
+                      child:
+                      Constants.mediumWhiteTextWidget('S1ME01', 20, Colors.white)
+                  ),
                 ),
-              ),
-             // Constants.mediumWhiteTextWidget('${nickName}', 18, Colors.white),
 
-              SizedBox(height: 32,),
-              Constants.regularWhiteTextWidget('Email', 14, Constants.grayTextColor),
-
-              SizedBox(height: 2,),
-              Container(
-                // width: Constants.screenWidth(context),
-                // height: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center ,
-                  children: [
-                    Center(
-                      child:Constants.mediumWhiteTextWidget('${email}', 18, Colors.white),
-                    ),
-                    SizedBox(width: 8,),
-                    GestureDetector(onTap: (){
-                      print('1234');
-                    },
-                      child:Image(image: AssetImage('images/profile/user_edit_info_icon.png'),width: 13,),
-                    )
-                  ],
+                Container(
+                  width: Constants.screenWidth(context)- 140,
+                  height: 1,
+                  margin: EdgeInsets.only(top: 30),
+                  color: Color.fromRGBO(76, 78, 90, 1),
                 ),
-              ),
+
+                SizedBox(height: 32,),
+                Constants.regularWhiteTextWidget('NickName', 14, Constants.grayTextColor),
+
+                SizedBox(height: 5,),
+
+                Container(
+                  // width: Constants.screenWidth(context),
+                  // height: 200,
+                  child: Row(
+
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center ,
+                    children: [
+
+                      Container(
+                        height:26 ,
+                        width: boundingTextSize(context, '${nickName}', TextStyle(color: Colors.white,fontSize: 18,fontFamily: 'SanFranciscoDisplay')).width+8,
+                        child: TextField(
+                          cursorColor: Color.fromRGBO(248, 98, 21, 1),
+                          onChanged: (value){
+                            nickName = value;
+                            setState(() {});
+                          },
+                          controller: _nickController,
+                          style: TextStyle(color: Colors.white), // 设置字体颜色
+                          decoration: InputDecoration(
+                            border: InputBorder.none, // 移除边框
+                            hintText: '${nickName}',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent), // 设置焦点时的边框颜色
+                            ),
+                            hintStyle: TextStyle(
+                                color: Color.fromRGBO(255, 255, 255, 1.0),
+                                fontFamily: 'SanFranciscoDisplay',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        // child:Constants.mediumWhiteTextWidget('${nickName}', 18, Colors.white),
+                      ),
+                      SizedBox(width: 8,),
+                      GestureDetector(onTap: (){
+                        print('1234');
+                      },
+                        child:Image(image: AssetImage('images/profile/user_edit_info_icon.png'),width: 13,),
+                      )
+                    ],
+
+                  ),
+                ),
+                // Constants.mediumWhiteTextWidget('${nickName}', 18, Colors.white),
+
+                SizedBox(height: 32,),
+                Constants.regularWhiteTextWidget('Email', 14, Constants.grayTextColor),
+
+                SizedBox(height: 5,),
+                Container(
+                  // width: Constants.screenWidth(context),
+                  // height: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center ,
+                    children: [
+                      Container(
+                        height:22 ,
+                        width: boundingTextSize(context, '${email}', TextStyle(color: Colors.white,fontSize: 18,fontFamily: 'SanFranciscoDisplay')).width + 8,
+                        child: TextField(
+                          cursorColor: Color.fromRGBO(248, 98, 21, 1),
+                          onChanged: (value){
+                            email = value;
+                            setState(() {});
+                          },
+                          controller: _emailController,
+                          style: TextStyle(color: Colors.white), // 设置字体颜色
+                          decoration: InputDecoration(
+                            border: InputBorder.none, // 移除边框
+                            hintText: '${email}',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent), // 设置焦点时的边框颜色
+                            ),
+                            hintStyle: TextStyle(
+                                color: Color.fromRGBO(255, 255, 255, 1.0),
+                                fontFamily: 'SanFranciscoDisplay',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        // child:Constants.mediumWhiteTextWidget('${email}', 18, Colors.white),
+                      ),
+                      SizedBox(width: 8,),
+                      GestureDetector(onTap: (){
+                        print('1234');
+                      },
+                        child:Image(image: AssetImage('images/profile/user_edit_info_icon.png'),width: 13,),
+                      )
+                    ],
+                  ),
+                ),
 
 
-             // Constants.mediumWhiteTextWidget('${email}', 18, Colors.white),
+                // Constants.mediumWhiteTextWidget('${email}', 18, Colors.white),
 
-              SizedBox(height: 32,),
-              Constants.regularWhiteTextWidget('Serial Number', 14, Constants.grayTextColor),
+                SizedBox(height: 32,),
+                Constants.regularWhiteTextWidget('Serial Number', 14, Constants.grayTextColor),
 
-              SizedBox(height: 2,),
-              Constants.mediumWhiteTextWidget('S1ME01', 18, Colors.white),
+                SizedBox(height: 2,),
+                Constants.mediumWhiteTextWidget('S1ME01', 18, Colors.white),
 
-              SizedBox(height: 32,),
-              Constants.regularWhiteTextWidget('Version', 14, Constants.grayTextColor),
+                SizedBox(height: 32,),
+                Constants.regularWhiteTextWidget('Version', 14, Constants.grayTextColor),
 
-              SizedBox(height: 2,),
-              Constants.mediumWhiteTextWidget('1.1.1#000000', 18, Colors.white),
-            ],
+                SizedBox(height: 2,),
+                Constants.mediumWhiteTextWidget('1.1.1#000000', 18, Colors.white),
+              ],
 
+            ),
           ),
         ),
+
       ),
     );
 
