4144c6be6171c043fb0d81b3fe46502c0bb44b5b
App攻城狮 <1215933389@qq.com>
2025-01-01 11:32:47 +0800

    连接成功调试弹窗
    连接成功调试弹窗去除

--- lib/utils/blue_tooth_manager.dart
+++ lib/utils/blue_tooth_manager.dart
@@ -181,7 +181,7 @@ class BluetoothManager {
           // }
         }
         // 连接成功弹窗
-        EasyLoading.showSuccess('Bluetooth connection successful');
+      //  EasyLoading.showSuccess('Bluetooth connection successful');
         // 监听数据
         _ble.subscribeToCharacteristic(notifyCharacteristic).listen((data) {
           print("deviceId =${model.device.id}---上报来的数据data = $data");
