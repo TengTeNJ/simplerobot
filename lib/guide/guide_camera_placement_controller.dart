02ea6f39510a3e3c4fcfdfb906c9397591f41b7e
App攻城狮 <1215933389@qq.com>
2025-02-22 12:58:24 +0800

    引导页面
    引导页面

---
+++ lib/guide/guide_camera_placement_controller.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tennis_robot/route/routes.dart';
+import 'package:tennis_robot/utils/navigator_util.dart';
+
+import '../constant/constants.dart';
+
+class GuideCameraPlacementController extends StatefulWidget {
+  const GuideCameraPlacementController({super.key});
+
+  @override
+  State<GuideCameraPlacementController> createState() => _GuideCameraPlacementControllerState();
+}
+
+class _GuideCameraPlacementControllerState extends State<GuideCameraPlacementController> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]); // 设置横屏模式
+    return Scaffold(
+      backgroundColor: Constants.darkControllerColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(onTap: (){
+                NavigatorUtil.push(Routes.guideCameraPlacementPage);
+                print('点击进行下一步');
+              },
+                child: Container(
+                  margin: EdgeInsets.only(top: 32,left: 32),
+                  width: 52,
+                  height: 52,
+                  color: Constants.selectedModelOrangeBgColor,
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 32,right: 32),
+                width: 52,
+                height: 52,
+                color: Constants.selectedModelOrangeBgColor,
+              ),
+            ],
+          ),
+
+          Constants.mediumWhiteTextWidget('Camera Placement', 36, Colors.white),
+          SizedBox(height: 25,),
+          Center(
+            child: Image(
+                width: 233,
+                height: 130,
+                image: AssetImage('images/home/under_way.png')),
+          ) ,
+          SizedBox(height: 25,),
+          Constants.regularWhiteTextWidget('Place the phone mount at either end of the tennis court net,'
+              ' and position the camera towards the direction of the court you wish to capture, '
+              'ensuring that the mount is set higher than the net.', 16, Colors.white),
+
+
+        ],
+
+      ),
+    );
+
+  }
+}
