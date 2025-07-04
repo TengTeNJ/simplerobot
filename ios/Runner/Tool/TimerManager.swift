b90559a2ef6aad78fbb2d826688764b594cecf9c
App攻城狮 <1215933389@qq.com>
2025-07-04 19:48:55 +0800

    定时器管理器更新
    定时器管理器更新

--- ios/Runner/Tool/TimerManager.swift
+++ ios/Runner/Tool/TimerManager.swift
@@ -9,35 +9,42 @@
 import Foundation
 
 class TimerManager {
-    // 定义一个 Timer 属性
     private var timer: Timer?
-    
-    // 定义一个闭包，用于定时器触发时执行的任务
-    private var timerTask: (() -> Void)?
-    
-    // 初始化方法
-    init() {}
-    
-    // 启动定时器
-    func startTimer(interval: TimeInterval, task: @escaping () -> Void) {
-        // 保存任务闭包
-        self.timerTask = task
-        
-        // 创建并启动定时器
-        self.timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
-            self?.timerTask?()
+    private var startTime: Date?
+    private var elapsedTime: TimeInterval = 0
+
+    // 创建并开始一个定时器
+func startTimer(interval: TimeInterval, repeats: Bool, action: @escaping () -> Void) {
+        stopTimer() // 停止当前定时器（如果存在）
+        startTime = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: repeats) { [weak self] _ in
+            guard let self = self else { return }
+            self.elapsedTime += interval
+            action()
         }
     }
-    
-    // 停止定时器
+
+    // 暂停定时器
+    func pauseTimer() {
+        guard let timer = timer else { return }
+        timer.invalidate()
+        self.timer = nil
+        elapsedTime = -startTime!.timeIntervalSinceNow
+    }
+
+    // 继续定时器
+    func resumeTimer(interval: TimeInterval, repeats: Bool, action: @escaping () -> Void) {
+        stopTimer()
+        startTime = Date(timeIntervalSinceNow: elapsedTime)
+        startTimer(interval: interval, repeats: repeats, action: action)
+    }
+
+    // 停止并销毁定时器
     func stopTimer() {
-        // 无效化定时器
         timer?.invalidate()
         timer = nil
-    }
-    
-    // 销毁定时器管理器时，确保停止定时器
-    deinit {
-        stopTimer()
+        startTime = nil
+        elapsedTime = 0
     }
 }
+
